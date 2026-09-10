edc754b693adae01530a5ef2fd1c1052ae4fe0bd
Mathis Roquillon <mat.roquillon@gmail.com>
2026-09-10 10:26:02 +0200

    commit de exo3

---
+++ exo3.py
@@ -0,0 +1,11 @@
+dic={'t' : 3, 'kg' : 2, 'g' : 1}
+def convertion(poids : int, unite : str, unite2 : str) : 
+    if unite in dic.keys():
+        number=dic[unite]
+    if unite == "kg" and unite2 == "g":
+        poids=poids*1000
+    elif unite == "g" and unite2 == "kg":
+        poids=poids/1000
+    print (poids)
+    
+convertion(45, "g", "kg")
